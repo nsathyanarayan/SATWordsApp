580586077927c81823d8fd88b1cd6748a8e8d02c
nsathyanarayan <PrajwalPranav@gmail.com>
2018-10-25 22:37:04 -0700

    added flashcards

--- UI/src/app/dashboard/dashboard.component.ts
+++ UI/src/app/dashboard/dashboard.component.ts
@@ -1,43 +1,58 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {enableProdMode} from '@angular/core';
 import { SATWordDefinition } from '../SATWordDefinition';
 import { DictionaryService} from '../DictionaryService';
 
+// Similar to a class. If we only need type checking, the interface is sufficient and lighter weight.
+interface Flashcard {
+    question: string;
+    answer: string;
+    source: string;
+    sourceUrl: string;
+}
 
+// decorator syntax
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
+// export component class
 export class DashboardComponent implements OnInit {
 
-  SATWords: SATWordDefinition[] = [];
-  public show = true;
-    private currIndex = 0;
-    private current = [];
-    @Input() index: number;
+    SATWords: SATWordDefinition[] = [];
+    public cardIndex = Math.floor((Math.random() * this.SATWords.length));
+    public cardOrderIndex = [];
+    public answerHidden = true;
 
-  constructor(private ws: DictionaryService) { }
+    constructor(private ws: DictionaryService) { }
 
-  ngOnInit() {
-    this.getWords();
-  }
+    ngOnInit() {
+      this.getWords();
+    }
 
-  getWords(): void {
-    this.ws.getWords().subscribe(words => this.SATWords = words);
-   }
+    getWords(): void {
+      this.ws.getWords().subscribe(words => this.SATWords = words);
+     }
 
-  getCurrActive(index) {
-      return true;
+    showAnswer() {
+        this.answerHidden = false;
+    }
+
+    goToNextCard() {
+        this.cardOrderIndex.push(this.cardIndex);
+        this.cardIndex = Math.floor((Math.random() * this.SATWords.length));
+        this.answerHidden = true;
+    }
+
+    goToPrevCard() {
+        if (this.cardOrderIndex.length > 0) {
+            this.cardIndex = this.cardOrderIndex.pop();
+            this.answerHidden = true;
+        }
+    }
 }
 
-showPanel(type) {
-  if ( type === 'next') {
-    this.currIndex += 1;
-  } else if (this.currIndex > 0 && type === 'prev') {
-      this.currIndex -= 1;
-  }
-}
 
-
-}
